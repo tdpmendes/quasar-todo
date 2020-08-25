94677e88e991e32d19f2952bebb086693e1277c6
Tácio Mendes <tacio.mendes@gmail.com>
2020-08-25 18:38:27 -0300

    The entire project

---
+++ src/pages/Help.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Mussum Ipsum, cacilds vidis litro abertis. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Interagi no mé, cursus quis, vehicula ac nisi.</p>
+    <p>Atirei o pau no gatis, per gatis num morreus. Mé faiz elementum girarzis, nisi eros vermeio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.</p>
+    
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
